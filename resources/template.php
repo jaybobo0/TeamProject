9773cb5f93d918fc58d853a252073607791a5f62
361786 <119968352+JamesWhitlyIsh@users.noreply.github.com>
2023-01-20 19:33:42 +0000

    Finished the template

--- resources/template.php
+++ resources/template.php
@@ -1,3 +1,4 @@
+<?php /*1st Line on every webpage.*/ include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,28 +7,11 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Heroic Features - Start Bootstrap Template</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Bootstrap icons-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="/resources/templateFiles/css/styles.css" rel="stylesheet" />
+      <?php echo $cssFiles; ?>
     </head>
     <body>
-        <!-- Responsive navbar-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container px-lg-5">
-                <a class="navbar-brand" href="#!">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+      <!-- Nav bar -->
+       <?php echo $nav ?>
         <!-- Header-->
         <header class="py-5">
             <div class="container px-lg-5">
@@ -104,11 +88,9 @@
         </section>
         <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
-        </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="templateFiles/js/scripts.js"></script>
+          <!-- footer -->
+          <?php echo $footer; ?>
+          
+        <?php echo $jsFiles; ?>
     </body>
 </html>
