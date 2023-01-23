d99840ec6e2df25480779b45ddbc47109af2838e
Jayden Black <119446626+jaybobo0@users.noreply.github.com>
2023-01-23 19:16:08 +0000

    changed the session name to title

--- redirects/task.php
+++ redirects/task.php
@@ -1,11 +1,25 @@
 <?php $_SERVER['DOCUMENT_ROOT'];
 
+
+
   // Get data from json file.
   $jsonTasks = file_get_contents($_SERVER['DOCUMENT_ROOT']."/data/tasks.json"); 
   $jsonUsers = file_get_contents($_SERVER['DOCUMENT_ROOT']."/data/users.json"); 
   // Convert json data to PHP array.
   $tasksData =  json_decode($jsonTasks, TRUE);   
   $usersData =  json_decode($jsonUsers, TRUE);   
+
+
+
+$_SESSION["form"]["title"] = $_POST['title'];
+$_SESSION["form"]["last-name"] = $_POST['last-name'];
+$_SESSION["form"]["email"] = $_POST['email'];
+$_SESSION["form"]["phone-number"] = $_POST['phone-number'];
+$_SESSION["form"]["relationship-status"] = $_POST['relationship-status'];
+
+
+
+
 
   echo '<pre>';
   var_dump($tasksData);
