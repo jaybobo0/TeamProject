04cd3f36171da6cdeaa160a2c939852a39bd99c3
361786 <113467417+Jomeish@users.noreply.github.com>
2023-03-06 18:31:14 +0000

    made list

--- test.php
+++ test.php
@@ -1,30 +1,35 @@
 <?php /*1st Line on every webpage.*/ include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; 
 
 
+$sortedTaskUserData = $mergedTaskUserData;
+
+
+// function sortByReward($a,$b){
+//   if ($a['reward'] == $b['reward']){
+//     return 0;
+//   }
+//   return ($a['reward'] < $b['reward']) ? 1 : -1; // 1 if true -1 if false
+// }
+
+
+usort($sortedTaskUserData, 'sortByReward');
 
 
 
-function sortByReward($a,$b){
-  if ($a['reward'] == $b['reward']){
-    return 0;
-  }
-  return ($a['reward'] < $b['reward']) ? 1 : -1; // 1 if true -1 if false
-}
-
-usort($mergedTaskUserData, 'sortByReward');
 
 
-
-  //   echo '<pre>';
-  // var_dump($mergedTaskUserData);
-  // echo '</pre>';
-
-// foreach($mergedTaskUserData as $task){
-  
+// foreach($sortedTaskUserData as $task){
+  $topThreeRewards = [];
 
   $x=0;
 while($x <= 2) {
-  echo $mergedTaskUserData[$x]['reward'] . '<br>';
+  // echo $sortedTaskUserData[$x]['reward'] . '<br>';
+  $topThreeRewards[$x] = $sortedTaskUserData[$x];
+  
  $x++;
  }
+
+  echo '<pre>';
+  var_dump($topThreeRewards);
+  echo '</pre>';
 ?>
