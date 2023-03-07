4c887dc98769afd111f0e54477480b6c2cca1ea8
361786 <113467417+Jomeish@users.noreply.github.com>
2023-03-07 19:20:14 +0000

    statused

--- redirects/status.php
+++ redirects/status.php
@@ -1,16 +1,26 @@
 <?php /*1st Line on every webpage.*/ include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; 
 
-foreach($tasksData as $task) { //foreach start
+if (isset($_GET['uid'])) {
+   foreach($mergedTaskUserData as $task) { //foreach start
   if ($task['uid'] == $_GET['uid']){ //if start
     $_SESSION['statusUpdate'] = $task;
   }//if end
  
 }//foreach end
 
+// header('Location: /tasks/status.php');
+  
+} else if (isset($_POST['BTNstatus'])){
+  echo 'its already set';
+} else {
+  echo 'wy are you here >:(';
+}
+
+
 
 // echo '<pre>';
 // var_dump($_SESSION['statusUpdate']);
 // echo '</pre>';
 
-header('Location: /tasks/status.php');
+
 ?>
--- tasks/status.php
+++ tasks/status.php
@@ -1,9 +1,9 @@
 <?php /*1st Line on every webpage.*/ include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; 
 
 
-echo '<pre>';
-var_dump($_SESSION['statusUpdate']);
-echo '</pre>';
+// echo '<pre>';
+// var_dump($_SESSION['statusUpdate']);
+// echo '</pre>';
 
 ?>
 <!DOCTYPE html>
@@ -25,8 +25,43 @@ echo '</pre>';
                 <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
                     <div class="m-4 m-lg-5">
                         <h1 class="display-5 fw-bold">Edit Status</h1>
-                        <p class="fs-4">Bootstrap utility classes are used to create this jumbotron since the old component has been removed from the framework. Why create custom CSS when you can use utilities?</p>
-                        <a class="btn btn-primary btn-lg" href="#!">Call to action</a>
+                        <form action="" method="post">
+                          <label>Update Status</label>
+                          <br>
+                          <select>
+                            <option value="0">hatooy</option>
+                            <option value="0">habluey</option>
+                            <option value="0">hablgueeuey</option>
+                          </select>
+
+                          <input class="btn btn-primary btn-lg" type="submit" value="submit" name="BTNstatus" action="/redirects/status.php"/>
+                        
+
+                        </form>
+                      <table class="table table-hover">
+                        <thead>
+                          <tr>
+                            <th scope="col">User</th>
+                            <th scope="col">Catagory</th>
+                            <th scope="col">Due Date</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Value</th>
+                            <th scope="col">Status</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <?php 
+                              echo '<td>'.$_SESSION['statusUpdate']['fName'].'</td>';
+                              echo '<td>'.$_SESSION['statusUpdate']['categories'].'</td>';
+                              echo '<td>'.$_SESSION['statusUpdate']['dateDeadline'].'</td>';
+                              echo '<td>'.$_SESSION['statusUpdate']['title'].'</td>';
+                              echo '<td>'.$_SESSION['statusUpdate']['reward'].'</td>';
+                              echo '<td>'.$_SESSION['statusUpdate']['categories'].'</td>';
+                            ?>                                                                  
+                          </tr>
+                        </tbody>
+                      </table>
                     </div>
                 </div>
             </div>
@@ -35,62 +70,7 @@ echo '</pre>';
         <section class="pt-4">
             <div class="container px-lg-5">
                 <!-- Page Features-->
-                <div class="row gx-lg-5">
-                    <div class="col-lg-6 col-xxl-4 mb-5">
-                        <div class="card bg-light border-0 h-100">
-                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
-                                <h2 class="fs-4 fw-bold">Fresh new layout</h2>
-                                <p class="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-xxl-4 mb-5">
-                        <div class="card bg-light border-0 h-100">
-                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-cloud-download"></i></div>
-                                <h2 class="fs-4 fw-bold">Free to download</h2>
-                                <p class="mb-0">As always, Start Bootstrap has a powerful collectin of free templates.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-xxl-4 mb-5">
-                        <div class="card bg-light border-0 h-100">
-                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-card-heading"></i></div>
-                                <h2 class="fs-4 fw-bold">Jumbotron hero header</h2>
-                                <p class="mb-0">The heroic part of this template is the jumbotron hero header!</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-xxl-4 mb-5">
-                        <div class="card bg-light border-0 h-100">
-                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-bootstrap"></i></div>
-                                <h2 class="fs-4 fw-bold">Feature boxes</h2>
-                                <p class="mb-0">We've created some custom feature boxes using Bootstrap icons!</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-xxl-4 mb-5">
-                        <div class="card bg-light border-0 h-100">
-                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-code"></i></div>
-                                <h2 class="fs-4 fw-bold">Simple clean code</h2>
-                                <p class="mb-0">We keep our dependencies up to date and squash bugs as they come!</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-xxl-4 mb-5">
-                        <div class="card bg-light border-0 h-100">
-                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-patch-check"></i></div>
-                                <h2 class="fs-4 fw-bold">A name you trust</h2>
-                                <p class="mb-0">Start Bootstrap has been the leader in free Bootstrap templates since 2013!</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <div
             </div>
         </section>
         <!-- Footer-->
